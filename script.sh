79ccd5692793b28517ef967041d161722b2912f3
Standinwol <toantrum9x@gmail.com>
2024-04-05 23:23:34 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ do
     echo "This is the ${i}th loop"
 
     # Run yarn command
-    ore --rpc https://mainnet.helius-rpc.com/?api-key=e25b1bbd-1eaf-4141-8a06-9db33d1250c5 --keypair ~/.config/solana/id.json claim
+    ore --rpc https://mainnet.helius-rpc.com/?api-key=e25b1bbd-1eaf-4141-8a06-9db33d1250c5 --keypair ~/.config/solana/id.json --priority-fee 5000 claim
 
     # Check whether the command was executed successfully
     if [ $? -ne 0 ]; then
